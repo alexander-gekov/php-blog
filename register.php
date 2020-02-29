fa5f1671be8feda7c036975b730a7ffbdc59c6c0
Stratulat,Dragos-Radu D <427178@student.fontys.nl>
2020-02-29 12:20:11 +0100

    Added registration, login, logout and password reset.

---
+++ register.php
@@ -0,0 +1,120 @@
+<?php
+require_once "connection.php";
+
+$username = $password = $confirm_password = "";
+$username_err = $password_err = $confirm_password_err = "";
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Please enter a username.";
+    } else{
+        $sql = "SELECT id FROM users WHERE username = ?";
+
+        if($stmt = mysqli_prepare($link, $sql)){
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+
+            $param_username = trim($_POST["username"]);
+
+            if(mysqli_stmt_execute($stmt)){
+                mysqli_stmt_store_result($stmt);
+
+                if(mysqli_stmt_num_rows($stmt) == 1){
+                    $username_err = "This username is already taken.";
+                } else{
+                    $username = trim($_POST["username"]);
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            mysqli_stmt_close($stmt);
+        }
+    }
+
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter a password.";
+    } elseif(strlen(trim($_POST["password"])) < 6){
+        $password_err = "Password must have atleast 6 characters.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+
+    if(empty(trim($_POST["confirm_password"]))){
+        $confirm_password_err = "Please confirm password.";
+    } else{
+        $confirm_password = trim($_POST["confirm_password"]);
+        if(empty($password_err) && ($password != $confirm_password)){
+            $confirm_password_err = "Password did not match.";
+        }
+    }
+
+    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
+
+        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+
+        if($stmt = mysqli_prepare($link, $sql)){
+            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
+
+            $param_username = $username;
+            $param_password = $password;
+
+            if(mysqli_stmt_execute($stmt)){
+                header("location: login.php");
+            } else{
+                echo "Something went wrong. Please try again later.";
+            }
+
+            mysqli_stmt_close($stmt);
+        }
+    }
+
+    mysqli_close($link);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sign Up</title>
+    <link rel="stylesheet" href="style.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <h2>Register</h2>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div>
+              <p>
+                <b>Username</b>
+                <input type="text" name="username" value="<?php echo $username; ?>">
+                <span><?php echo $username_err; ?></span>
+              </p>
+            </div>
+            <div>
+              <p>
+                <b>Password</b>
+                <input type="password" name="password" value="<?php echo $password; ?>">
+                <span><?php echo $password_err; ?></span>
+              </p>
+            </div>
+            <div>
+              <p>
+                <b>Confirm Password</b>
+                <input type="password" name="confirm_password" value="<?php echo $confirm_password; ?>">
+                <span><?php echo $confirm_password_err; ?></span>
+              </p>
+            </div>
+            <div>
+                <input type="submit" class="button" value="Submit">
+                <input type="reset" class="button" value="Clear">
+            </div>
+            <p>Already have an account? <a href="login.php">Login here</a>.</p>
+        </form>
+    </div>
+</body>
+</html>
--- welcome.php
+++ welcome.php
@@ -1,4 +1,14 @@
+<?php
+session_start();
+
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+?>
+
 <!DOCTYPE html>
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -9,11 +19,12 @@
 <body>
   <header class="header">
     <a href="/" class="logo">BLOG</a>
+    <a class="greet">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to the blog.</a>
     <input class="menu-btn" type="checkbox" id="menu-btn" />
     <label class="menu-icon" for="menu-btn"><span class="nav-icon"></span></label>
     <ul class="menu">
-        <li><a href="login.php">Login</a></li>
-        <li><a href="register.php">Register</a></li>
+        <li><a href="reset.php">Reset password</a></li>
+        <li><a href="logout.php">Logout</a></li>
     </ul>
 </header>
 </body>
